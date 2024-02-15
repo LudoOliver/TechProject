56ee1be079e993aeba4727de7775499a95aa8df7
xd21736 <98823093+xd21736@users.noreply.github.com>
2024-02-15 18:23:38 +0000

    EthnoAttempt

--- EthoAttempt.py
+++ EthoAttempt.py
@@ -12,7 +12,16 @@ y={}
 for i in x:
     for j in pyetho.country_languages(country_name=i):
         if j in y:
-            y[j] += i
+            y[j].append(i)
         else: 
-            y.update({j,i})
+            y.update({j:[i]})
             
+Languages = y.keys()
+
+Pops = dict.fromkeys(Languages)
+for i in Languages:
+    temp = [0]
+    for a in y[i]:
+        #Pops[i] 
+        temp.append((pyetho.language_population(country_name=a,lang=i)))
+    Pops.update({i:temp})
